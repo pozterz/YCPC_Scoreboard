aff4d81d6f5935c914fa4bbca8e69149379116b0
5635512083 <5635512083>
2016-11-14 21:16:12 +0700

    finish config

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>YCPC 2016</title>
+	<link rel="shortcut icon" href="images/favicon.ico">
 	<link rel="stylesheet" href="css/bulma.css">
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
@@ -14,30 +15,46 @@
 	<section class="hero is-dark is-large">
 		<div class="hero-head">
 			<div class="container has-text-centered">
-				<h1 class="title">
+			<hr/>
+				<h1 class="title"><br/>
 					YCPC Contest 2016
 				</h1>
+
 				<h2 class="subtitle">
-					Prince of Songkla University Phuket Campus.
+					Prince of Songkla University, Phuket Campus.
 				</h2>
-				<div class="columns">
-					<div class="column">
-						<img src="http://www.barcampsongkhla.org/img/sponsor/sponsor-logo-0.jpg" width="128px"  height="128px">
-						<img src="http://www.barcampsongkhla.org/img/sponsor/sponsor-logo-0.jpg" width="128px"  height="128px">
-						<img src="images/psu-logo.png" width="128px"  height="128px">
-					</div>
-				</div>
+				<hr/>
 			</div>
 		</div>
 	</section>
 	<div class="container">
+		<div class="has-text-centered">
+			<div class="columns">
+					<div class="column">
+						<img src="images/psu-logo.png" width="94px" height="128px">
+						<img src="images/coe-logo.png" width="128px" height="128px">
+						<img src="images/cnr_wide_logo.png" width="246px" >
+					</div>
+				</div></div>
 		<table class="table is-striped">
-			<caption>contest explaination</caption>
+			<caption> 
+				<hr/>
+					<h2 class="tag is-warning is-medium">FIRST TO SOLVE PROBLEM</h2>
+					<h2 class="tag is-success is-medium">SOLVED PROBLEM</h2>
+					<h2 class="tag is-danger is-medium">ATTEMPTED PROBLEM</h2>
+					<h2 class="tag is-info is-medium">PENDING JUDGEMENT</h2>
+				<hr/>
+			</caption>
 			<thead id="header">
 			</thead>
 			<tbody id="data">
 			</tbody>
 		</table>
+	</div>
+	<div class="container">
+		<div class="notification is-info">
+		  รบกวนทำแบบสอบถามด้วยครับที่ <a href="https://goo.gl/forms/1CyQHvOMZyIV6eCa2" target="_blank">https://goo.gl/forms/1CyQHvOMZyIV6eCa2</a> ขอบคุณครับ :D
+		</div>
 	</div>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ $(document).ready(function(){
 	function getData(){
 		$.ajax({
 			method: "GET",
-  			url: "http://localhost/ycpc/data.php",
+  			url: "http://172.26.116.159/ycpc/data.php",
   			dataType: "json",
   			success:function(response){
   				var $head = $('<tr>');
 
+  				$head.append($('<th>').text('Rank').css({color:"#fff",background:"#4a4a4a"}));
   				$head.append($('<th>').text('Name').css({color:"#fff",background:"#4a4a4a"}));
   				$head.append($('<th>').text('Solved').css({color:"#fff",background:"#4a4a4a"}));
   				$head.append($('<th>').text('Time').css({color:"#fff",background:"#4a4a4a"}));
@@ -16,8 +17,8 @@ $(document).ready(function(){
   						$head
   							.append(
   								$('<th>')
-  								.html('<i class="fa fa-star fa-2x"></i>')
-  								.css({"text-align":"center",background:"#4a4a4a",color:response.contestStandings.standingsHeader[0].colorList[0].colors[0].problem[i].$.color})
+  								.html('<i class="fa fa-star-o fa-2x" ></i>')
+  								.css({"text-align":"center","background-color":response.contestStandings.standingsHeader[0].colorList[0].colors[0].problem[i].$.color,color:"white"})
   							);
   				}
 
@@ -25,7 +26,7 @@ $(document).ready(function(){
 
   				$.each(response.contestStandings.teamStanding,function(i,team){
   					var $tr = $('<tr>');
-
+  					$tr.append($('<th>').text(team.$.rank));
   					$tr.append($('<th>').text(team.$.teamName));
   					$tr.append($('<th>').text(team.$.solved));
   					$tr.append($('<th>').text(team.$.points));
@@ -50,7 +51,7 @@ $(document).ready(function(){
 	  						}else{
 	  							
 	  							if($bestsolutiontime == $solutionTime){
-  									$color="is-warning";
+  									$color="is-warning solved";
   								} else {
   									$color = "is-success";
   								}
@@ -73,6 +74,30 @@ $(document).ready(function(){
   					
   					$('#data').append($tr);
   				});
+
+  					
+
+            $tr = $('<tr>');
+            $tr.append($('<td colspan="4">').text('Solved / Tries'));
+
+            for(var i = 0;i < response.contestStandings.standingsHeader[0].$.problemCount;i++){
+              $summary =  response.contestStandings.standingsHeader[0].problem[i].$;
+              $tr.append(
+                    $('<td class="has-text-centered">')
+                    .text(
+                      $summary.numberSolved
+                      +"/"
+                      + $summary.attempts
+                      +"("
+                      + Math.round(($summary.numberSolved/($summary.attempts == 0 && $summary.numberSolved == 0 ? 1 : $summary.attempts))*100)
+                      +")"
+                      +"%"
+                      )
+                  );
+            }
+
+            $('#data').append($tr);
+
   			}
 		})
 	}
